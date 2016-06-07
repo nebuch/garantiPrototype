c389081c2182324491f6d2080826aaf1f69bc05d
nebuch <dogancihad@gmail.com>
2016-06-07 19:20:36 +0300

    crosshair

---
+++ GarantiPrototype/Assets/Scripts/crosshairRotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class crosshairRotate : MonoBehaviour {
+
+	public float _speed;
+	private float _rotationZ;
+	public RectTransform _image;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		_image.rotation = Quaternion.Euler (0, 0, _rotationZ);
+
+		_rotationZ += _speed * Time.unscaledDeltaTime;
+	}
+}
