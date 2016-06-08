bf6544ee6f25f87e6351ad932ee2c50d7109dcdd
nebuch <dogancihad@gmail.com>
2016-06-09 01:22:43 +0300

    Crosshair lighten'

---
+++ GarantiPrototype/Assets/Scripts/crosshairRotate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class crosshairRotate : MonoBehaviour {
+
+	public float _speed;
+	private float _zRotation;
+	private RectTransform _image;
+
+	// Use this for initialization
+	void Start () {
+		_image = this.GetComponent<RectTransform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		_zRotation -= _speed * Time.unscaledDeltaTime;
+		_image.localRotation = Quaternion.Euler (0, 0, _zRotation);
+
+	}
+}
