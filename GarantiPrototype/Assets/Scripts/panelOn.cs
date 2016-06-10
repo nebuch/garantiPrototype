4f52a863b54d36a2ad4205d4e86062305deb6d08
nebuch <dogancihad@gmail.com>
2016-06-10 15:47:58 +0300

    Major UI commit

---
+++ GarantiPrototype/Assets/Scripts/panelOn.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class panelOn : MonoBehaviour {
+	public bool _on;
+	public Image _panelImage;
+	public Text _panelText;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (!_on) {
+		
+			_panelImage.enabled = false;
+			_panelText.enabled = false;
+		
+		}
+
+		if (_on) {
+
+			_panelImage.enabled = true;
+			_panelText.enabled = true;
+
+		}
+
+	}
+
+	public void PanelOn(){
+	
+		_on = true;
+	
+	}
+}
